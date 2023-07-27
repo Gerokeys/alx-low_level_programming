7d73d65274b8b8356c589b21b2d999c441000b4b
Gerokeys <shikukugerald111@gmail.com>
2023-07-27 14:10:27 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,8 +3,8 @@
 /**
  * add_node_end - adds a new node at the end
  * of a list_t list.
- * @head: The head of the linked list.
- * @str: The string to store in the list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
  * Return: address of the head.
  */
 
@@ -37,4 +37,5 @@ list_t *add_node_end(list_t **head, const char *str)
 		current_node->next = new_node;
 	}
 
+	return (*head);
 }
