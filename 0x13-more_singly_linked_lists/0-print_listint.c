33ed160413a12a213b2682fc079b1c042fb3d314
Gerokeys <shikukugerald111@gmail.com>
2023-08-01 12:57:16 -0700

    The first commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,8 +2,10 @@
 
 /**
  *   * print_listint - prints all the elements of a listint_t list
- *    * @h: pointer to head of list
+ *    *
+ *    *@h: pointer to head of list
  *     * Return: number of elements
+ *
  *      */
 size_t print_listint(const listint_t *h)
 {
---
+++ 0x13-more_singly_linked_lists/listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *
+ * listint_len - returns the number of elements in a
+ *
+ * linked listint_t list
+ * *
+ * @h: Thepointer to head of list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	register size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+}
